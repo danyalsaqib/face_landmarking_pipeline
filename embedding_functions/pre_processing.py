1c2a221f36ba9b8de4922d6f1221bb6f8bbbb894
Danyal Saqib <dsaqib.bee17seecs@seecs.edu.pk>
2021-12-12 16:41:40 +0500

    Update pre_processing.py

--- embedding_functions/pre_processing.py
+++ embedding_functions/pre_processing.py
@@ -14,8 +14,29 @@ from helper_functions import warp_and_crop_face, get_reference_facial_points
 
 # Only function to be called is preprocess_image
 
+
+# Extracting landmarks from RetinaFace output
+# This function outputs x and y coordinates of all landmarks, relative to bounding box values.
+# This is because our embedding generation module needs relative coordinates.
+def retinaface_landmarking(faces, x_start = 0, y_start = 0):
+    points = []
+    points.append(faces['face_1']['landmarks']['right_eye'][0] - x_start)
+    points.append(faces['face_1']['landmarks']['left_eye'][0] - x_start)
+    points.append(faces['face_1']['landmarks']['nose'][0] - x_start)
+    points.append(faces['face_1']['landmarks']['mouth_right'][0] - x_start)
+    points.append(faces['face_1']['landmarks']['mouth_left'][0] - x_start)
+    points.append(faces['face_1']['landmarks']['right_eye'][1] - y_start)
+    points.append(faces['face_1']['landmarks']['left_eye'][1] - y_start)
+    points.append(faces['face_1']['landmarks']['nose'][1] - y_start)
+    points.append(faces['face_1']['landmarks']['mouth_right'][1] - y_start)
+    points.append(faces['face_1']['landmarks']['mouth_left'][1] - y_start)
+    return points
+
 def preprocess_image_embed(out2):
-    crop_img, points = out2
+    crop_img, faces, x_start, y_start = out2
+    points = retinaface_landmarking(faces, x_start, y_start)
+    for i in range(len(points)):
+        points[i] = int(points[i])
     output_size=(224 , 224)
     default_square = True
     inner_padding_factor = 0.25
